6b98dc7d1770279a91f70b21535131c6902e6e75
certihimo <kalidahm3d@gmail.com>
2023-06-22 09:36:03 +0300

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    has_many :post 
+    has_one :Profile
 end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    belongs_to :author
+    has_manym :post_tags
+    has_many :tags, through: :post_tags
 end
